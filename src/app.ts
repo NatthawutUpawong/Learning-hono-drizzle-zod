f31fda88588372f0cbedaaacf6b3288ef37a66c0
Chen Aupawong <chenaupawong@MacBook-Air-khxng-Chen.local>
2025-01-26 21:11:24 +0700

    add new project learning

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { error } from 'console'
+import { logger } from 'hono/logger'
+import { notFound, onError } from 'stoker/middlewares'
+import { pinoLogger } from './middlewares/pino-logger.js'
+
+const app = new OpenAPIHono()
+
+app.use(pinoLogger())
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get("/error", (c) => {
+    c.status(422)
+    c.var.logger.info("Wow!")
+    throw new Error("Ho no!")
+})
+
+app.notFound(notFound)
+app.onError(onError)
+
+export default app
