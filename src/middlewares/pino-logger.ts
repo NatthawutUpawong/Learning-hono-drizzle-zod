ca09639bfc63932131dd26c118f1a26bc56096a6
Chainchinjung <upawongnatthawut@gmail.com>
2025-01-27 09:52:18 +0700

    update pino-logger.ts

--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -5,47 +5,12 @@ import env from "./env.js";
 
 
 export function pinoLogger() {
-    return logger({
-        pino: pino(
-            {
-                level: env.LOG_LEVEL || "info",
-            },
-            import { OpenAPIHono } from '@hono/zod-openapi'
-            import { error } from 'console'
-            import { logger } from 'hono/logger'
-            import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares'
-            import { pinoLogger } from './middlewares/pino-logger.js'
-            import { PinoLogger } from 'hono-pino'
-            
-            
-            
-            interface AppBindings{
-              Variables: {
-                logger: PinoLogger
-              }
-            }
-            
-            const app = new OpenAPIHono<AppBindings>()
-            
-            app.use(serveEmojiFavicon("📝"))
-            app.use(pinoLogger())
-            
-            app.get('/', (c) => {
-              return c.text('Hello Hono! hello')
-            })
-            
-            app.get("/error", (c) => {
-                c.status(422)
-                c.var.logger.debug("")
-                throw new Error("Ho no!")
-            })
-            
-            app.notFound(notFound)
-            app.onError(onError)
-            
-            export default appenv.NODE_ENV === "production" ? undefined : pretty()),
-        http: {
-            reqId: () => crypto.randomUUID(),
-        }
-    })
+  return logger({
+    pino: pino({
+      level: env.LOG_LEVEL || "info",
+    }, env.NODE_ENV === "production" ? undefined : pretty()),
+    http: {
+      reqId: () => crypto.randomUUID(),
+    },
+  });
 }
